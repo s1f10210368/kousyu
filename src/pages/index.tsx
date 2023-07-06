3578bc5e268c065dcbfb583ab1c28d16ec657ddf
s1f10210254 <iniad@DESKTOP-LC3J6L9.localdomain>
2023-07-06 17:39:22 +0900

    bombMap時々１０にならないが完成かつボードの条件考え中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  //const bomCount = 10;
+  const bomCount = 10;
 
   // 0 -> ボムなし
   // 1 -> ボム有
@@ -35,7 +35,7 @@ const Home = () => {
   ]);
 
   //ゲーム開始
-  const isPlaying = userInput.some((row) => row.some((input) => input !== 0));
+  //const isPlaying = userInput.some((row) => row.some((input) => input !== 0));
   //爆発
   //const isFailure = userInputs.some((row, y) =>
   //  row.some((input, x) => input === 1 && bombMap[y][x] === 1)
@@ -62,46 +62,74 @@ const Home = () => {
     [-1, 1],
   ];
 
+  //押した場所周囲８方向を探索し隣接している数字を表示する関数
+  const check = (x: number, y: number, direction: number[]) => {
+    //bombMapから
+    const [dx, dy] = direction;
+    const tempX = x + dx;
+    const tempY = y + dy;
+    let bom_count = 0;
+
+    if (bombMap[tempX][tempY] === 1) {
+      bom_count = 1;
+    }
+    return bom_count;
+    console.log(bom_count);
+  };
+
   //for (){
   //board + directions + userInput +
 
+  //userInputが１の場合のみtrue（ボム作成時に使用）
+  const jaj = userInput.flat().filter((input) => input === 1).length === 1;
+
   const onClick = (x: number, y: number) => {
     console.log(x, y);
+
+    //boardを初期状態ー１に設定する
+    for (let i = 0; i < userInput.length; i++) {
+      const row: number[] = [];
+      for (let j = 0; j < userInput[i].length; j++) {
+        row.push(-1);
+      }
+      board.push(row);
+    }
 
     //ユーザのクリックに応じてuserInputの値を更新
     const updatedUserInput = [...userInput];
     updatedUserInput[y][x] = 1;
     setUserInput(updatedUserInput);
 
-    if (isPlaying) {
-      //userInptが0のマスの座標を取得
-      const emptyCells = [];
-      //userInput.lengthは９
-      for (let i = 0; i < userInput.length; i++) {
-        for (let j = 0; j < userInput[i].length; j++) {
-          if (userInput[i][j] === 0) {
-            emptyCells.push([j, i]);
-          }
+    //ボムの配置
+    if (jaj) {
+      const updatedBombMap = [...bombMap];
+      let count = 0;
+      while (count < bomCount) {
+        const randomX = Math.floor(Math.random() * 9);
+        const randomY = Math.floor(Math.random() * 9);
+        if (randomX !== x && randomY !== y && updatedBombMap[randomY][randomY] !== 1) {
+          updatedBombMap[randomY][randomX] = 1;
+          count++;
         }
+        setBombMap(updatedBombMap);
       }
-      //ランダムに１０個の座標を取得
-      const randomCells = [];
-      while (randomCells.length < 10 && emptyCells.length > 0) {
-        //０以上１未満のランダムな数値を生成。
-        const randomIndex = Math.floor(Math.random() * emptyCells.length);
-        //emptyCellsからランダムなインデックスに対応する要素を削除し、その要素をrandomCellという変数に代入する
-        const randomCell = emptyCells.splice(randomIndex, 1)[0];
-        randomCells.push(randomCell);
-      }
-
-      //bombMapの該当座標を１に変更
-      const updatedBomMap = [...bombMap];
-      for (const [x, y] of randomCells) {
-        updatedBomMap[y][x] = 1;
-      }
-      setBombMap(updatedBomMap);
-      console.log(updatedBomMap);
     }
+    console.log(bombMap);
+
+    //押した場所にボムマップのボムがある場合１１をボードに返す
+    if (bombMap[y][x] === 1) {
+      board[y][x] = 11;
+      console.log('ゲーム終了');
+    }
+
+    //directionsで格納した各方向から隣接している数をチェック
+    let count = 0;
+    for (const direction of directions) {
+      count = check(x, y, direction);
+    }
+    //boardの中身を隣接する数に変更
+    board[y][x] = count;
+    console.log(board);
   };
 
   return (
