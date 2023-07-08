c7bd5bd85c485a9ff511111b97da280254f5c92d
s1f10210254 <iniad@DESKTOP-LC3J6L9.localdomain>
2023-07-08 18:09:00 +0900

    空白連鎖処理未完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,18 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const board: number[][] = [];
+  const board = [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+  ];
+
   //ゲーム開始
   //const isPlaying = userInput.some((row) => row.some((input) => input !== 0));
   //爆発
@@ -53,6 +64,7 @@ const Home = () => {
 
   //8方向辞書
   const directions = [
+    //左から反時計回り
     [-1, 0],
     [-1, -1],
     [0, -1],
@@ -63,7 +75,20 @@ const Home = () => {
     [-1, 1],
   ];
 
-  //押した場所周囲８方向を探索し隣接している数字を表示する関数
+  // console.log('board1');
+  // console.table(board);
+
+  // for (let s = 0; s < 9; s++) {
+  //   for (let t = 0; t < 9; t++) {
+  //     if (board[s][t] === -1) {
+  //       board[s][t] = 0;
+  //     }
+  //   }
+  // }
+  // console.log('board2');
+  // console.table(board);
+
+  //押した場所周囲８方向を探索し隣接している数字を返す関数
   const check = (x: number, y: number) => {
     let bombCount = 0;
     for (const [dx, dy] of directions) {
@@ -78,6 +103,74 @@ const Home = () => {
     return bombCount;
   };
 
+  //boardの作成
+  const createBoard = () => {
+    for (let y = 0; y < userInput.length; y++) {
+      for (let x = 0; x < userInput[y].length; x++) {
+        if (userInput[y][x] === 1) {
+          //ボムあるときゲームオーバー
+          if (bombMap[y][x] === 1) {
+            board[y][x] = 11;
+            console.log('ゲームオーバー');
+          }
+          // ボムないとき
+          else {
+            //隣接するボムを計測
+            const count = check(x, y);
+            //隣接するボムが０の時
+            if (count === 0) {
+              //空白連鎖処理
+              board[y][x] = 0;
+              for (const [dx, dy] of directions) {
+                const nx = x + dx;
+                const ny = y + dy;
+                if (nx >= 0 && nx < bombMap[0].length && ny >= 0 && ny < bombMap.length) {
+                  while (check(nx, ny) !== 0) {
+                    userInput[dy][dx] = 1;
+                    board[dy][dx] = 0;
+                  }
+                }
+              }
+            }
+
+            //隣接するボムがあるとき
+            else {
+              board[y][x] = count;
+            }
+          }
+        } else {
+          board[y][x] = -1;
+        }
+      }
+    }
+    return board;
+  };
+
+  //隣接するボムが０の時ユーザーインプットを１に更新
+  // const expandBlank = (x: number, y: number) => {
+  //   if (userInput[y][x] === 0) {
+  //     userInput[y][x] = 1;
+
+  //     for (const [dx, dy] of directions) {
+  //       const nx = x + dx;
+  //       const ny = y + dy;
+  //       if (nx >= 0 && nx < bombMap[0].length && ny >= 0 && ny < bombMap.length) {
+  //         if (userInput[ny][nx] === 0) {
+  //           userInput[ny][nx] = 1;
+  //           const count = check(nx, ny);
+  //           if (count === 0) {
+  //             expandBlank(nx, ny);
+  //           } else {
+  //             userInput[ny][nx] = 1;
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
+
+  createBoard();
+
   //userInputが１の場合のみtrue（ボム作成時に使用）
   const jaj = userInput.flat().filter((input) => input === 1).length === 1;
 
@@ -88,6 +181,7 @@ const Home = () => {
     const updatedUserInput = [...userInput];
     updatedUserInput[y][x] = 1;
     setUserInput(updatedUserInput);
+    console.table(userInput);
 
     //ボムの配置
     if (jaj) {
@@ -103,38 +197,22 @@ const Home = () => {
         setBombMap(updatedBombMap);
       }
     }
-    console.log('BombMap');
+    console.log('BombMap↓');
     console.table(bombMap);
 
-    //board作成
-    for (let y = 0; y < updatedUserInput.length; y++) {
-      const row: number[] = [];
-      for (let x = 0; x < updatedUserInput[y].length; x++) {
-        if (updatedUserInput[y][x] === 1) {
-          if (bombMap[y][x] === 1) {
-            row.push(11); //ボムセル
-            console.log('ゲームオーバー');
-          } else {
-            const count = check(y, x);
-            row.push(count);
-          }
-        } else {
-          row.push(-1); //石
-        }
-      }
-      board.push(row);
-    }
-    console.log('Board');
+    //boardを作成
+    const board = createBoard();
+    console.log('Board↓');
     console.table(board);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-        {userInput.map((row, y) =>
+        {board.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
-              {color !== 0 && (
+              {color !== -1 && (
                 <div
                   className={styles.stone}
                   //style={{ background: color === 1 ? '#000' : '#fff' }}
